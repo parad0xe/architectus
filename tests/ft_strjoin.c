4097a373ae5c78c999d7e627e10c9992b9e9f19f
leonardecavele <leonard.decavele@gmail.com>
2025-11-09 23:10:52 +0100

    new: ft_strjoin

---
+++ tests/ft_strjoin.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldecavel <ldecavel@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/09 23:08:03 by ldecavel          #+#    #+#             */
+/*   Updated: 2025/11/09 23:08:28 by ldecavel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "tester.h"
+#include <stdlib.h>
+
+void	test1(void)
+{
+	char	*s;
+
+	set_description("Join two normal strings.");
+	s = ft_strjoin("hello", "world");
+	check_is_equal(STR, s, "helloworld");
+	free(s);
+}
+
+void	test2(void)
+{
+	char	*s;
+
+	set_description("Join with an empty second string.");
+	s = ft_strjoin("hello", "");
+	check_is_equal(STR, s, "hello");
+	free(s);
+}
+
+void	test3(void)
+{
+	char	*s;
+
+	set_description("Join with an empty first string.");
+	s = ft_strjoin("", "world");
+	check_is_equal(STR, s, "world");
+	free(s);
+}
+
+void	test4(void)
+{
+	char	*s;
+
+	set_description("Join two empty strings.");
+	s = ft_strjoin("", "");
+	check_is_equal(STR, s, "");
+	free(s);
+}
+
+void	test5(void)
+{
+	ft_strjoin(NULL, "world");
+}
+
+void	test6(void)
+{
+	ft_strjoin("hello", NULL);
+}
+
+int	main(void)
+{
+	handle(&test1);
+	handle(&test2);
+	handle(&test3);
+	handle(&test4);
+	handle_sigsegv("NULL first string should segfault", &test5, NO_SEGFAULT);
+	handle_sigsegv("NULL second string should segfault", &test6, NO_SEGFAULT);
+}
+
