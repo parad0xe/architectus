1e4c971a27f8758889e61737cd38989e327c2798
leonardecavele <leonard.decavele@gmail.com>
2025-11-09 22:54:32 +0100

    new: ft_strmapi

---
+++ tests/ft_strmapi.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldecavel <ldecavel@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/09 22:52:20 by ldecavel          #+#    #+#             */
+/*   Updated: 2025/11/09 22:54:18 by ldecavel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "tester.h"
+#include <stdlib.h>
+
+static char	mapi_func(unsigned int i, char c)
+{
+	return (c + i);
+}
+
+static char	mapi_upper(unsigned int i, char c)
+{
+	(void)i;
+	if (c >= 'a' && c <= 'z')
+		return (c - 32);
+	return (c);
+}
+
+void	test1(void)
+{
+	char	*s;
+	char	src[] = "abcd";
+
+	set_description("Apply function that shifts each character by its index.");
+	s = ft_strmapi(src, mapi_func);
+	check_is_equal(STR, s, "aceg");
+	free(s);
+}
+
+void	test2(void)
+{
+	char	*s;
+	char	src[] = "hello";
+
+	set_description("Apply uppercase function to all characters.");
+	s = ft_strmapi(src, mapi_upper);
+	check_is_equal(STR, s, "HELLO");
+	free(s);
+}
+
+void	test3(void)
+{
+	ft_strmapi(NULL, mapi_func);
+}
+
+void	test4(void)
+{
+	char	src[] = "test";
+
+	ft_strmapi(src, NULL);
+}
+
+int	main(void)
+{
+	handle(&test1);
+	handle(&test2);
+	handle_sigsegv("NULL string should segfault", &test3, NO_SEGFAULT);
+	handle_sigsegv("NULL function should segfault", &test4, NO_SEGFAULT);
+}
